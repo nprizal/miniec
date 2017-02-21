e041cb825b8e7b1fd713b0b265e3311ca193d295
lbynaka+005 <lbynaka+005@gmail.com>
2017-02-21 05:23:20 -0500

    remove event_configs

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170214211050) do
+ActiveRecord::Schema.define(version: 20170221102154) do
 
   create_table "event_categories", force: :cascade do |t|
     t.text     "name"
     t.text     "memo"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "event_configs", force: :cascade do |t|
-    t.text     "title"
-    t.text     "subtitle"
-    t.text     "stylename"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
