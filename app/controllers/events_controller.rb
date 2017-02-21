3d20560a4f8b22996c4960f505a26da790aa765e
lbynaka+005 <lbynaka+005@gmail.com>
2017-02-21 07:53:01 -0500

    remove pager

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,28 +3,13 @@ class EventsController < ApplicationController
 
 	# ECサイト：イベント一覧
 	def index
-		page_size = 5
-		@page_num = 0
-		if params[:page] != nil then
-			@page_num = params[:page].to_i
-		end
 		@data = EventPost.all.order('created_at desc')
-			.offset(page_size * @page_num)
-			.limit(page_size)
 	end
 
 	# ECサイト：カテゴリ別
 	def category
-		page_size = 5
-		@page_num = 0
-		if params[:page] != nil then
-			@page_num = params[:page].to_i
-		end
 		@event_category = EventCategory.find params[:id]
-		
 		@data = EventPost.where('event_category_id = ?',params[:id])
-			.order('created_at desc').offset(page_size * @page_num)
-			.limit(page_size)
 	end
 
 	# ECサイト:イベント詳細
